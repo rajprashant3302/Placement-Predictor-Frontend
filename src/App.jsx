0d537e742309e41a0cd69f96ea1fed7e459dd101
rajprashant3302 <rajprashant3302@gmail.com>
2025-10-06 01:28:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -118,7 +118,7 @@ const handleClosePrediction = () => {
     );
 
     try {
-      const response = await fetch("http://localhost:5000/predict", {
+      const response = await fetch("https://placement-predictor-backend.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ features: featureArray }),
