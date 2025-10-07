2912e1dbe717778671481c021ad7a240a3c9bcac
rajprashant3302 <rajprashant3302@gmail.com>
2025-10-07 19:05:08 +0530

    msg

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ function App() {
 
     try {
       // const response = await fetch("https://placement-predictor-backend.onrender.com/predict", {
-      const response = await fetch("http://localhost:5000/predict", {
+      const response = await fetch("https://placement-predictor-backend.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify( features )
